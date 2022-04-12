f067e71a628348f09e7c547fd9add59eab03f277
Guillaume A <guillaume@codr.fr>
2022-04-12 08:20:00 +0200

    Update 03-post-install.sh

--- 03-post-install.sh
+++ 03-post-install.sh
@@ -4,3 +4,9 @@ systemctl start NetworkManager
 nmcli dev wifi connect network-ssid password "network-password"
 
 useradd -G wheel username
+passwd Joky
+
+pacman -Sy vim git
+export EDITOR=/usr/bin/vim
+visudo
+
