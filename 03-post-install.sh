c95e774c3b23049d89fb5c0b3396389a06266926
Guillaume A <guillaume@codr.fr>
2022-04-12 09:46:13 +0200

    Update 03-post-install.sh

--- 03-post-install.sh
+++ 03-post-install.sh
@@ -6,7 +6,59 @@ nmcli dev wifi connect network-ssid password "network-password"
 useradd -G wheel username
 passwd Joky
 
-pacman -Sy vim git
+pacman -Sy vim git openssh
 export EDITOR=/usr/bin/vim
 visudo
+
+pacman -Sy 
+man 
+xf86-video-{vesa,intel} 
+xorg-{serveur,xinit}
+xorg-fonts-100dpi
+
+zsh
+alacritty
+nvidia 
+
+xorg-xbacklight
+arandr
+feh
+dmenu
+
+
+...acpi
+...acpid
+
+docker
+ 
+systemctl enable docker
+systemctl start docker
+usermod -aG docker $USER
+newgrp docker
+
+
+Clone dwm repo
+sudo make clean install
+
+Clone this repo
+./dotfiles.sh
+./share.sh
+
+
+chsh -s /usr/bin/zsh
+
+Installer ohmyzsh
+
+
+-----
+
+Fichier a rajouter dans le repo
+
+.zshrc
+
+-------
+
+Aur
+
+Brave-bin
 
